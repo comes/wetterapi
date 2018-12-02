5548eaa4f4d95f8343fe30985ac7a71696599f9c
jeremias wolff <jeremiaswolff@gmail.com>
2018-12-02 19:46:20 +0100

    new rain data

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -7,12 +7,20 @@ use App\Archive;
 
 class WeatherController extends Controller
 {
-    public function weather() {
-	$pageSize = request()->get('pageSize',500);
-    	return Archive::paginate($pageSize);
+    public function weather()
+    {
+        $pageSize = request()->get('pageSize', 500);
+        return Archive::paginate($pageSize);
     }
 
-    public function current() {
-       	return Archive::first();
+    public function current()
+    {
+        /** @var Collection */
+        $data = Archive::first()->toArray();
+        $data = array_add($data, 'rain24h', with(new Archive)->getRainPast24h());
+        $data = array_add($data, 'rainCurrentMonth', with(new Archive)->getRainCurrentMonth());
+        $data = array_add($data, 'rainLastMonth', with(new Archive)->getRainLastMonth());
+
+        return $data;
     }
 }
